d99b6ba6e0e7e8d3ccd74ab22ce98f1e273b5607
nomadmtb <kylegluce@gmail.com>
2013-10-31 11:52:46 -0700

    testing seed

--- DoiServer/db/seeds.rb
+++ DoiServer/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Adding Users to the Database.
+User.create([{ email: 'kyle@gmail.com' }, { password: 'password' }, { password_confirmation: 'password' }])
