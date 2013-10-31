b82d7307a1b0c57e9de2c26d8fd324ddd19ae378
nomadmtb <kylegluce@gmail.com>
2013-10-31 14:42:24 -0700

    Edited seeds

--- DoiServer/db/seeds.rb
+++ DoiServer/db/seeds.rb
@@ -9,3 +9,11 @@
 # Adding Users to the Database.
 puts 'Adding Users'
 kyle = User.create :email => 'kyle@gmail.com', :password => 'password', :password_confirmation => 'password'
+frank = User.create :email => 'frank@gmail.com', :password => 'password', :password_confirmation => 'password'
+annie = User.create :email => 'annie@gmail.com', :password => 'password', :password_confirmation => 'password'
+mike = User.create :email => 'mike@gmail.com', :password => 'password', :password_confirmation => 'password'
+melissa = User.create :email => 'melissa@gmail.com', :password => 'password', :password_confirmation => 'password'
+doi_1 = Doi.create :name => '1876545672', :user_id => 1, :description => 'DOI for my paper on funny hats'
+url_1 = Url.create :url => 'http://www.google.com', :doi_id => 1
+url_2 = Url.create :url => 'http://www.wikipedia.com', :doi_id => 1
+url_3 = Url.create :url => 'http://www.wsj.com', :doi_id => 1
